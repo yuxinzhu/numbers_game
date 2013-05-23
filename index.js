e16202653d8fbba090c92680b0fe61133033c323
Yuxin Zhu <zhuyxn@gmail.com>
2013-05-22 19:32:34 -0700

    basic working calculator using angular

---
+++ index.js
@@ -0,0 +1,102 @@
+function calculatorController($scope) {
+    $scope.GPA = 0;
+    $scope.majorGPA = 0;
+
+    $scope.letterGrades = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F", "P", "NP"];
+    $scope.courses = [
+        {name: "COMPSCI 170", letterGrade: "B-", units: 4, major: true, gradePoint: 2.7*4, hide: false},
+        {name: "COMPSCI 188", letterGrade: "A-", units: 4, major: true, gradePoint: 3.3*4, hide: false},
+        {name: "PLANTBI 13", letterGrade: "A", units: 3, major: false, gradePoint: 4.0*3, hide: false}
+    ];
+
+    $scope.init = function () {
+        $scope.updateGPA();
+    };
+
+
+    $scope.letterToGPA = function(letterGrade) {
+        var GPADict = {
+            "A+": 4.0, "A": 4.0, "P": 0, "A-": 3.7,
+            "B+": 3.3, "B": 3.0, "B-": 2.7,
+            "C+": 2.3, "C": 2.0, "C-": 1.7,
+            "D": 1.0, "F": 0, "NP": 0
+        };
+        return GPADict[letterGrade];
+    };
+
+    $scope.addCourse = function() {
+        $scope.courses.push({
+            name: $scope.courseName,
+            letterGrade: $scope.letterGrade,
+            units: $scope.courseUnits,
+            major: $scope.majorBoolean,
+            hide: false,
+            gradePoint: $scope.letterToGPA($scope.letterGrade) * $scope.courseUnits,
+        });
+        $scope.gpa = $scope.updateGPA();
+        $scope.courseName = '';
+    }
+
+    $scope.updateGPA = function() {
+        var tempMajor = $scope.calculateGPA(true);
+        var temp = $scope.calculateGPA(false);
+
+        if (tempMajor != null){
+            $scope.majorGPA = tempMajor;
+        } else { $scope.majorGPA = 0; };
+
+        if (temp != null){
+            $scope.GPA = temp;
+        } else { $scope.GPA = 0; };
+    };
+
+    $scope.calculateGPA = function(majorGPA) {
+        console.log("hello!");
+        var relevantCourses = _.filter($scope.courses, function(course) { return course.letterGrade != "P" && course.letterGrade != "NP" && course.hide == false});
+        if(majorGPA){
+            relevantCourses = _.filter(relevantCourses, function(course) { return course.major == true });
+        }
+        var totalGrade = _.reduce(relevantCourses, function(memo, course){ return memo + course.gradePoint; }, 0);
+        var totalUnits = _.reduce(relevantCourses, function(memo, course){ return memo + course.units; }, 0);
+        return totalGrade/totalUnits;
+    };
+
+  $scope.removeCourse = function(course) {
+    $scope.courses.splice($scope.courses.indexOf(course), 1);
+    $scope.updateGPA();
+  };
+
+  $scope.incrementCourse = function(course) {
+    var index = $scope.letterGrades.indexOf(course.letterGrade);
+    console.log(index);
+    if (index != 0) {
+      course.letterGrade = $scope.letterGrades[--index];
+      course.gradePoint = $scope.letterToGPA(course.letterGrade) * course.units;
+      $scope.updateGPA();
+    };
+  };
+
+  $scope.decrementCourse = function(course) {
+    var index = $scope.letterGrades.indexOf(course.letterGrade);
+    console.log(index);
+    if (index != $scope.letterGrades.length-1) {
+      course.letterGrade = $scope.letterGrades[++index];
+      course.gradePoint = $scope.letterToGPA(course.letterGrade) * course.units;
+      $scope.updateGPA();
+    };
+  };
+
+  $scope.hideCourse = function(course) {
+    course.hide = !course.hide;
+    $scope.updateGPA();
+  };
+  //add course
+  //delete course
+  //discount course
+  //increase GPA
+  //decrease GPA
+  //show technical
+  //show nottechnical
+  //total Courses
+  //tabs for technical, nontechnical, all
+};
